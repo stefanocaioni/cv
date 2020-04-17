837cd18840d8a49157934f964aec1aa859037951
Stefano Caioni <ste.caioni@gmail.com>
2020-04-17 17:20:58 +1000

    Webpack code splitting

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
-);
+import(/* webpackChunkName: 'app' */ './App')
+.then(({ default: App }) =>
+  ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+  )
+)
 
