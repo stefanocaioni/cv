6ba62433350085b8920b8406d804ba182c2b460a
Stefano Caioni <ste.caioni@gmail.com>
2020-04-22 16:22:48 +1000

    Create redirects file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CreateFileWebpack = require('create-file-webpack');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
@@ -13,6 +14,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html'
+    }),
+    new CreateFileWebpack({
+      path: './dist',
+      fileName: '_redirects',
+      content: '/* /index.html 200'
     })
   ]
 }
